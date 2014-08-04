7bf1ae439305ab972d8b37fb4f47f0b4a1745039
Kevin Haines <kevin.haines@appsbroker.com>
2014-08-04 12:08:43 +0100

    Added predict2

--- problem210.c
+++ problem210.c
@@ -12,17 +12,27 @@ int func(int n) {
     return r;
 }
 
+int predict2(int n) {
+    int i, r = 0;
+    int c = (n*n*n - n)/2;
+
+    for (i=1; i < n; i++)
+        r += (i*(i+1)/2);
+    return c - r;
+}
+
 int predict(int n) {
     int i, r = 0;
     for (i=1; i < n; i++)
         r += ((n*(n+1)/2) - (i*(i+1)/2));
     return r;
 }
+
 int main(int argc, char **argv) {
 
     int n;
 
     for ( n = 3; n< 10; n++) {
-        printf("N is %d, predict: %d, result %d\n", n, predict(n), func(n));
+        printf("N is %d, predict: %d, result %d\n", n, predict2(n), func(n));
     }
 }
