18286811f6ffa3af3981ca780a337488edf4d5ff
Kevin Haines <kevin.haines@ntlworld.com>
2014-08-09 14:27:45 +0100

    Added types.h

---
+++ types.h
@@ -0,0 +1,11 @@
+#ifndef TYPES_H_INCLUDED
+#define TYPES_H_INCLUDED
+
+typedef unsigned char uint8;
+typedef int int32;
+typedef unsigned int uint32;
+typedef uint8 bool;
+#define true 1
+#define false 0
+
+#endif
