f67b31408348673b49871f11cfbbeb50b4fff122
Kevin Haines <kevin.haines@appsbroker.com>
2014-08-14 15:30:30 +0100

    Next problem

--- problem210-2.c
+++ problem210-2.c
@@ -12,18 +12,23 @@ int func(int n) {
     return r;
 }
 
-int predict2(int n) {
+int func3(int n) {
+    int i,j,k,l;
+    int r = 0;
+
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= i; j++)
+            for (k = j; k <= (i+j); k++)
+                for (l = 1; l <= (i+j-k); l++)
+                    r+=1;
+
+    return r;
+}
+
+int predict3(int n) {
     int i, r = 0;
     double r0, n0 = (double)n - 1;
     double c =(double)(n*n*n - n)/2;
-
-    for (i=1; i < n; i++)
-        r += (i*(i+1)/2);
-
-    r0 = (((n0*n0*n0 / 3) + ((n0*n0) / 2) + n0 / 6)+ ( n0*(n0+1)/2)) / 2;
-    printf("%f  %d ", r0, r);
-
-    return (int)(c - r0);
 }
 
 int predict(int n) {
@@ -33,8 +38,7 @@ int predict(int n) {
 int main(int argc, char **argv) {
 
     int n;
-
-    for ( n = 3; n< 10; n++) {
-        printf("N is %d, predict: %d, result %d\n", n, predict(n), func(n));
+    for ( n = 3; n<= 10; n++) {
+        printf("N is %d, predict: %d, result %d\n", n, predict(n), func3(n));
     }
 }
