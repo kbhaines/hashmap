fb88855808a853ab221b97fe34b36d33f8f411bc
Kevin Haines <kevin.haines@appsbroker.com>
2014-08-18 12:48:10 +0100

    Added func4

--- problem210-2.c
+++ problem210-2.c
@@ -25,6 +25,18 @@ int func3(int n) {
     return r;
 }
 
+int func4(int n) {
+    int i,j,k;
+    int r = 0;
+
+    for (i = 1; i <= n; i++)
+        for (j = i+1; j <= n; j++)
+            for (k = i+j-1; k <= n; k++)
+                r+=1;
+
+    return r;
+}
+
 int predict3(int n) {
     long p1 = (n*(n+1)/2);
     long r;
@@ -40,6 +52,6 @@ int main(int argc, char **argv) {
 
     int n;
     for ( n = 3; n<= 20; n++) {
-        printf("N is %d, predict: %d, result %d\n", n, predict3(n), func3(n));
+        printf("N is %d, predict: %d, result %d\n", n, predict3(n), func4(n));
     }
 }
