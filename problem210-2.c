d55a4df2f1de07865c12c22d5a20e9579350be66
Kevin Haines <kevin.haines@appsbroker.com>
2014-08-20 12:23:55 +0100

    Solved func4 problem

--- problem210-2.c
+++ problem210-2.c
@@ -29,13 +29,32 @@ int func4(int n) {
     int i,j,k;
     int r = 0;
 
-    for (i = 1; i <= n; i++)
-        for (j = i+1; j <= n; j++)
-            for (k = i+j-1; k <= n; k++)
+    for (i = 1; i <= n; i++) {
+        //printf("---i:%d", i);
+        for (j = i+1; j <= n; j++) {
+            //printf("  j:%d", j);
+            for (k = i+j-1; k <= n; k++) {
                 r+=1;
-
+                //printf("  k:%d, r:%d",k,r);
+            }
+        }
+        //printf("\n");
+    }
     return r;
 }
+
+#define MAX(p,q) ( (p) > (q) ? (p) : (q) )
+
+int predict4(int n) {
+    int j, i, k,s = 0;
+    for (i = 1; i <= n; i++) {
+        for (j = i+1; j <= n; j++) {
+            s += MAX(0,(n + 1) - (i+j-1));
+        }
+    }
+    return s;
+}
+
 
 int predict3(int n) {
     long p1 = (n*(n+1)/2);
@@ -52,6 +71,6 @@ int main(int argc, char **argv) {
 
     int n;
     for ( n = 3; n<= 20; n++) {
-        printf("N is %d, predict: %d, result %d\n", n, predict3(n), func4(n));
+        printf("N is %d, predict: %d, result %d\n", n, predict4(n), func4(n));
     }
 }
