260ba97f5f6c116cd588df05ae25e23197b199cc
Kevin Haines <kevin.haines@appsbroker.com>
2014-08-28 13:21:11 +0100

    working on webserver

--- web.c
+++ web.c
@@ -3,11 +3,18 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <string.h>
 #include <netinet/in.h>
 
 static int sock;
 
-void ws_init(void) {
+struct HttpRequest {
+
+    char *requestLine;
+
+};
+
+void wsInit(void) {
 
     int port = 8080;
     int backlog = 5;
@@ -33,11 +40,40 @@ void ws_init(void) {
 }
 
 
-int ws_accept() {
+int wsAccept() {
     
     struct sockaddr_in client_addr;
     int client_len = sizeof(client_addr);
     int newsock = accept(sock, (struct sockaddr *) &client_addr, &client_len);
 
     return newsock;
+}
+
+
+const char * HttpGetRequest(HttpRequest *req) {
+    return req->requestLine;
+}
+
+HttpRequest *wsGetRequest(int fd) {
+    
+    FILE *file = fdopen(fd, "r+");
+
+    HttpRequest *result = malloc(sizeof(HttpRequest));
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        int len = strlen(line);
+        if (len < 2) {
+            printf("Error: detected invalid line");
+            exit(1);
+        } else if (len == 2) {
+            return result;
+        }
+        // strip off the CR/LF characters
+        line[--len] = 0;
+        line[--len] = 0;
+        result->requestLine = strdup(line);
+        break;
+    }
+    fclose(file);
+    return result;
 }
--- web.h
+++ web.h
@@ -1,8 +1,14 @@
 #ifndef WEB_H_INCLUDED
 #define WEB_H_INCLUDED
 
-extern void ws_init(void);
+typedef struct HttpRequest HttpRequest;
 
-extern int ws_accept(void);
+extern void wsInit(void);
+
+extern int wsAccept(void);
+
+extern HttpRequest *wsGetRequest(int fd);
+
+extern const char * HttpGetRequest(HttpRequest *req);
 
 #endif
